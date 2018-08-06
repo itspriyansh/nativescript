2a8f7cacaca17d488d5b8646b83be940e9604f45
Priyansh Bhardwaj <priyanshbhj@gmail.com>
2018-08-06 16:07:21 +0530

    Sending Email

--- app/contact/contact.component.ts
+++ app/contact/contact.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as app from 'application';
 import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
+import { TNSFontIconService } from 'nativescript-ngx-fonticon';
+import * as Email from 'nativescript-email';
 
 @Component({
     selector: 'app-contact',
@@ -9,12 +11,25 @@ import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
 })
 
 export class ContactComponent implements OnInit{
-    constructor() { }
+    constructor(public fonticon: TNSFontIconService) { }
 
     ngOnInit() { }
     
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
+    }
+    sendEmail() {
+        Email.available()
+            .then((avail: boolean) => {
+                if(avail) {
+                    Email.compose({
+                        to: ['confusion@food.net'],
+                        subject: '[ConFusion]: Query',
+                        body: 'Dear Sir/Madam:'
+                    });
+                } else
+                    console.log('No Email Configured');
+            });
     }
 }
